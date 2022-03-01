b9ee8771213cd032e8ee6f88deaf766cdd27eaad
Mark <919128668@qq.com>
2022-03-01 21:28:05 +0800

    first commit

---
+++ src/main/java/com/df/Controller/hello.java
@@ -0,0 +1,15 @@
+package com.df.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class hello {
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(){
+        return "Hello,SpringBoot";
+    }
+
+}
